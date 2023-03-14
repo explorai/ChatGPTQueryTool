5aa277e09682c4d2b44d60d51f6df5d405b63a99
Felix Vaillancourt <marinefelx@gmail.com>
2023-03-14 15:38:19 -0400

    Added requirements, changes constructor and changed readme

--- README.md
+++ README.md
@@ -6,12 +6,43 @@ Create a `.env` file with your chatGPT token
 '''
 CHAT_GPT_TOKEN = 'your token here'
 '''
+
+Create a conda environment (optional)
+'''
+conda create --name (your environment name)
+'''
+
+Then install packages
+'''
+pip install -r requirements.txt
+'''
+## Batch processing
 In the chatGPT constructor, change the dataset path to your own dataset to pass through ChatGPT
 
-**Note : ** the initial data should be a csv with one single column or else some changes might be needed.
+**Note : ** the initial data should be a csv with one single column or else some changes to the code might be needed.
 
 Change context and query to your desired data treatment. 
 
 **Note : ** specify to ChatGPT to respond in only JSON or else the script will try to continuously query the api until it responds with correct JSON.
 
 Specify batch size in process method and export file path
+'''
+chat = chatGPTBot("gpt-3.5-turbo")
+chat.read("dataset/dataset.csv")
+context = "You are instructed to only respond in JSON format and you must provide synonyms to these car manufacturer options."
+query = "give me correct JSON format ONLY (ONLY JSON DATA, VERY IMPORTANT) with options as the keys and 1d array of 6 synonyms as value of this following array of vehicle manufacturer car options:"
+chat.process(60, query, context)
+chat.export("data/dataset_synonyms.csv", "csv")
+'''
+## whisper api call example
+
+'''
+chat = whisperGPTBot("whisper-1")
+print(chat.call("audio/Deep Learning In 5 Minutes.mp3"))
+'''
+
+## normal chatgpt api call example
+
+'''
+
+'''
--- main.py
+++ main.py
@@ -10,13 +10,16 @@ load_dotenv()
 
 
 class chatGPTBot:
-    def __init__(self, model, filename):
+    def __init__(self, model):
         self.GPT_token = os.getenv("CHAT_GPT_TOKEN")
         self.org = os.getenv("CHAT_GPT_ORG")
         self.model = model
-        self.data = pd.read_csv(filename)
         self.processed_df = None
         self.processed_JSON = None
+
+    def read(self, filename):
+        self.data = pd.read_csv(filename)
+        return
 
     def call(self, query, context=None):
         openai.api_key = self.GPT_token
@@ -29,21 +32,24 @@ class chatGPTBot:
         )
         return response
 
-    def process(self, batch, query, context=None):
+    def process(self, batch_size, query, context=None):
+        if self.data is None:
+            print("No data to process")
+            return
         raw_data = {}
         completion_tokens = 0
         prompt_tokens = 0
         total_tokens = 0
         processed_data = {"synonyms": {}}
         i = 0
-        while i < len(self.data) // batch:
+        while i < len(self.data) // batch_size:
             error = False
-            array = self.data[i * batch : (i + 1) * batch]
+            array = self.data[i * batch_size : (i + 1) * batch_size]
             array = array.to_numpy().flatten().tolist()
             array_string = "[" + ",".join(array) + "]"
             queryText = f"{query}:{array_string}"
             response = self.call(context, queryText)
-            print(f"Completed batch {i+1} of {len(self.data) // batch}")
+            print(f"Completed batch {i+1} of {len(self.data) // batch_size}")
             raw_data = {}
             raw_data[f"query{i}"] = (
                 response["choices"][0]["message"]["content"]
@@ -118,14 +124,26 @@ class whisperGPTBot:
         return text
 
 
+##batch
+# chat = chatGPTBot("gpt-3.5-turbo", "dataset/all_bb_distinct_options.csv")
+# context = "You are instructed to only respond in JSON format and you must provide synonyms to these car manufacturer options."
+# query = "give me correct JSON format ONLY (ONLY JSON DATA, VERY IMPORTANT) with options as the keys and 1d array of 6 synonyms as value of this following array of vehicle manufacturer car options:"
+# chat.process(60, query, context)
+# chat.export("data/dataset_synonyms_test_2.csv", "csv")
+
+##whisperAPI
+# chat = whisperGPTBot("whisper-1")
+# print(chat.call("audio/Deep Learning In 5 Minutes.mp3"))
+
+##chatAPI
+
+
 def main():
-    # chat = chatGPTBot("gpt-3.5-turbo", "dataset/all_bb_distinct_options.csv")
-    # context = "You are instructed to only respond in JSON format and you must provide synonyms to these car manufacturer options."
-    # query = "give me correct JSON format ONLY (ONLY JSON DATA, VERY IMPORTANT) with options as the keys and 1d array of 6 synonyms as value of this following array of vehicle manufacturer car options:"
-    # chat.process(60, query, context)
-    # chat.export("data/dataset_synonyms_test_2.csv", "csv")
-    chat = whisperGPTBot("whisper-1")
-    print(chat.call("audio/Deep Learning In 5 Minutes.mp3"))
+    chat = chatGPTBot("gpt-3.5-turbo")
+    query = "I want you to extract all medicals entity in this abstract. Organise your extraction by categories:Loss of p53's tumor-suppressive function, either via TP53 mutation or hyperactive p53 inhibitory proteins, is one of the most frequent events in the development of human cancer. Here, we describe a strategy of pharmacologically inhibiting iASPP, a negative regulator of p53, to restore wild-type p53's tumor-suppressive function. iASPP knockdown in the colon cancer cell line HCT116 efficiently promoted p53's transcriptional activity and induced p53-dependent cell death, suggesting a key role for iASPP in silencing p53 in this cell line. Screening of a preclinical and clinical drug library using isogenic HCT116 cell models revealed that cyclin-dependent kinase 9 (CDK9) inhibitors preferentially inhibit p53+/+, rather than p53-/-, cells. Mechanistically, CDK9 inhibitors downregulated iASPP at the transcriptional level. This downregulation was dose- and time-dependent. CDK9 inhibitors further showed synergistic effects in killing p53+/+ HCT116 cells when combined with the MDM2 inhibitor Nutlin-3. In a large TCGA pan-cancer cohort, iASPP overexpression predicted poor overall survival (OS) in wild-type p53 patients, with worse OS observed when MDM2 was simultaneously overexpressed. Our study identifies CDK9 inhibitors as p53-reactivating agents, and proposes a strategy to treat cancer by efficiently reactivating p53 via the concurrent inhibition of iASPP and MDM2."
+    response = chat.call(query)
+    with open("data/.json", "w") as outfile:
+        json.dump(response, outfile, indent=4)
 
 
 if __name__ == "__main__":
